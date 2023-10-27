9c65b0da2985b2df16407905e0876f039058a785
Pavanelli7 <vittor.ribeiro@sptech.school>
2023-10-27 15:07:17 -0300

    Configuração do ambiente de desenvolvimento

---
+++ bite-quest/src/App.jsx
@@ -0,0 +1,14 @@
+import Teste from "./components/Teste/Teste"
+
+Teste
+
+function App() {
+
+  return (
+    <>
+      <Teste />
+    </>
+  )
+}
+
+export default App
---
+++ bite-quest/src/components/Teste/Teste.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import * as S from "./style";
+
+function Teste() {
+  return (
+    <>
+      <S.Container>
+        <p>Teste</p>
+      </S.Container>
+    </>
+  )
+}
+
+export default Teste
