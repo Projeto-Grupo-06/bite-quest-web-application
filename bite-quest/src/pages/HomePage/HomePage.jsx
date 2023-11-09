0a0551cac6cbc57c7ca1798b74d1837667019471
Pavanelli7 <vittor.ribeiro@sptech.school>
2023-11-08 21:33:42 -0300

    link para cadastro

--- bite-quest/src/pages/HomePage/HomePage.jsx
+++ bite-quest/src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable no-irregular-whitespace */
 import * as S from "./styles"
-import { useNavigate } from 'react-router-dom';
 import LogoBlack from "../../assets/LogoBlack.png"
 import ButtonForm from "../../components/Form/buttons/ButtonForm/ButtonForm"
 import CardRestaurant from "../../components/CardRestaurant/cardRestaurant"
@@ -8,10 +7,11 @@ import SiSenor from "../../assets/siSenor.jpg"
 import Aguzzo from "../../assets/aguzzo.webp"
 import komeMoema from "../../assets/komeMoema.webp"
 import CocoBambu from "../../assets/cocoBambu.jpeg"
+import { useNavigate } from "react-router"
 
 function HomePage() {
-
   const navigate = useNavigate();
+
 
   return (
     <>
@@ -28,10 +28,10 @@ function HomePage() {
         <S.Wrapper>
           <S.WrapperUl>
             <li>
-              <ButtonForm onClick={() => navigate('/Login')} height="5.5vh" width="10vw" text="Entrar" colorDoText="black" fundoColor="white" marginTop="0px" fontSize="1rem" />
+              <ButtonForm height="5.5vh" width="10vw" text="Entrar" textColor="black" backgroundColor="white" fontSize="1.6rem" />
             </li>
             <li>
-              <ButtonForm onClick={() => navigate('/Cadastro')} height="5.5vh" width="10vw" text="Cadastre-se" colorDoText="whitesmoke" fundoColor="#FCA311" marginTop="0px" fontSize="1rem" />
+              <ButtonForm height="5.5vh" width="10vw" text="Cadastre-se" textColor="whitesmoke" backgroundColor="#FCA311" fontSize="1.6rem" />
             </li>
           </S.WrapperUl>
         </S.Wrapper>
@@ -51,7 +51,7 @@ function HomePage() {
           <S.PhirstP>
         Escolha seu favorito e cadastre-se para ter acesso a outras opções mais próximas de você.
             </S.PhirstP>
-            <ButtonForm height="11%" width="30%" text="Cadastrar" textColor="whitesmoke" backgroundColor="#FCA311" fontSize="1.7rem" />
+            <ButtonForm onClick={() => navigate('/Cadastro')} height="11%" width="30%" text="Cadastrar" textColor="whitesmoke" backgroundColor="#FCA311" fontSize="1.7rem" />
           </S.BoxRecommendations>
           <S.RestaurantRecomendations>
         <CardRestaurant height="95%" width="70%" imgUrl={SiSenor} fontSizeName="1.3rem" fontSizeAddress="1.1rem" name="si señor" address="São Paulo - Moema" colorCircle1="orange" colorCircle2="orange" colorCircle3="orange" colorCircle4="orange" colorCircle5="grey" />
