ba6692c2a4e8d45410dbe2ed85b3c8b64c60817e
VitorhugoSpG <vitor.varela@sptech.school>
2024-04-01 20:00:06 -0300

    att

--- bite-quest/vite.config.js
+++ bite-quest/vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  port : 80,
   server:{
-    host: "0.0.0.0"
+    host: "0.0.0.0",
+    port : 80,
+
   },
   plugins: [react()],
 })
