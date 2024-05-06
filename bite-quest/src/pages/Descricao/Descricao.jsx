890f9f5a95164550b9863bf7feb5cafecbea8bd3
VitorhugoSpG <vitor.varela@sptech.school>
2024-05-06 19:59:43 -0300

    inovação

--- bite-quest/src/pages/Descricao/Descricao.jsx
+++ bite-quest/src/pages/Descricao/Descricao.jsx
@@ -12,6 +12,7 @@ import HorariosRestaurante from "../../components/HorariosRestaurantes/HorariosR
 import CardComentarios from "../../components/CardComentarios/cardComentarios";
 import userPadrao from "../../assets/user.png";
 import api from "../../api";
+import { FaInstagram, FaWhatsapp } from 'react-icons/fa';
 
 function Descricao() {
   const navigate = useNavigate();
@@ -90,22 +91,30 @@ function Descricao() {
           </S.DivImagePrincipal>
 
           <S.DivInformacoes>
-            <S.iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3654.3803817482803!2d-46.6406595!3d-23.662351299999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce5ad2f2b78c05%3A0xb61ea425f8fd04f9!2sAv.%20Muzambinho%20-%20Jabaquara%2C%20S%C3%A3o%20Paulo%20-%20SP%2C%2004334-040!5e0!3m2!1spt-BR!2sbr!4v1701970809759!5m2!1spt-BR!2sbr"></S.iframe>
+  <S.iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3654.3803817482803!2d-46.6406595!3d-23.662351299999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce5ad2f2b78c05%3A0xb61ea425f8fd04f9!2sAv.%20Muzambinho%20-%20Jabaquara%2C%20S%C3%A3o%20Paulo%20-%20SP%2C%2004334-040!5e0!3m2!1spt-BR!2sbr!4v1701970809759!5m2!1spt-BR!2sbr"></S.iframe>
 
-            <S.DivAlinInformations>
-              <S.TittleDescricao>Descrição</S.TittleDescricao>
-              <S.TextoDescricao>{restaurante.descricao}</S.TextoDescricao>
+  <S.DivAlinInformations>
+    <S.TittleDescricao>Descrição</S.TittleDescricao>
+    <S.TextoDescricao>{restaurante.descricao}</S.TextoDescricao>
 
-              <S.TittleDescricao>Região </S.TittleDescricao>
-              <S.TextoDescricao>{restaurante.endereco}</S.TextoDescricao>
+    <S.TittleDescricao>Região </S.TittleDescricao>
+    <S.TextoDescricao>{restaurante.endereco}</S.TextoDescricao>
 
-              <S.TittleDescricao>Faixa de Preço</S.TittleDescricao>
-              <S.TextoDescricao>R$ 70 - R$ 120</S.TextoDescricao>
+    <S.TittleDescricao>Faixa de Preço</S.TittleDescricao>
+    <S.TextoDescricao>R$ 70 - R$ 120</S.TextoDescricao>
 
-              <S.TittleDescricao>MENU</S.TittleDescricao>
-              <S.img src={iconDownload}></S.img>
-            </S.DivAlinInformations>
-          </S.DivInformacoes>
+    <S.TittleDescricao>MENU</S.TittleDescricao>
+    <S.img src={iconDownload}></S.img>
+
+    <a href={`https://www.instagram.com/komemoema`}>
+  <FaInstagram size={32} />
+</a>
+<a href={`https://wa.me/11986167005`}>
+  <FaWhatsapp size={32} />
+</a>
+
+  </S.DivAlinInformations>
+</S.DivInformacoes>
         </S.DivUp>
 
         <S.DivDown>
